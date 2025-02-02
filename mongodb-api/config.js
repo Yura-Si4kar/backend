16f14e620d9747d12cf00efb70b849c989aabeab
Sichkar Yurii <si4kar2017@gmail.com>
2025-02-02 12:04:55 +0200

    Remove mongodb-api submodule configuration

--- mongodb-api/config.js
+++
@@ -1,4 +0,0 @@
-const categories = ['bbqs', 'bests', 'breads', 'burgers', 'chocolates', 'desserts', 'drinks', 'fried-chickens', 'ice-creams', 'pizzas', 'porks', 'sandwiches', 'sausages', 'steaks', 'foods'];
-const menuItems = ['tables', 'personnels', 'sales'];
-
-module.exports = {categories, menuItems};
